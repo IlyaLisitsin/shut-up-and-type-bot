14c2a521e02a6d3a5cf580e86af9df99dab38d5b
Ilya Savchuk <ilyas.sporichev@gmail.com>
2019-02-23 22:12:30 +0300

    trash rem

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const Telegraf = require('telegraf');
-const uuidv1 = require('uuid/v1');
 const fetch = require("node-fetch");
-const fs = require('fs')
-var Blob = require('blob');
-const OpusToPCM = require('opus-to-pcm')
 
-
-const token = '';
-const uuid = '';
-const yandexApiKey = '';
+const token = '742079108:AAGjJ9Ugxp2uN4CTEepcdcFwpbXoc20LqzE';
+const uuid = '4a99246551f24c339dc20c7a415f3cd6';
+const yandexApiKey = '6891b0ef-ea2f-421a-a761-be94d69393ff';
 
 // fs.readFile('./file_50.oga', (err, data) =>
 //     fetch(`https://asr.yandex.net/asr_xml?uuid=${uuid}&key=${yandexApiKey}&topic=queries&disableAntimat=false`, {
@@ -22,20 +17,14 @@ const yandexApiKey = '';
 //     })
 //         .then(kek => console.log(2, kek)))
 
-// https://asr.yandex.net/asr_xml?uuid=<идентификатор пользователя>&key=<API-ключ>&topic=<языковая модель>&lang=<язык>&disableAntimat=<антимат>
 const bot = new Telegraf(token);
-// bot.start((ctx) => ctx.reply('Welcome'));
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.on('message', (ctx) => {
     ctx.message.voice && fetch(`https://api.telegram.org/bot${token}/getFile?file_id=${ctx.message.voice.file_id}`)
         .then(response => response.json())
         .then(({ result: { file_path } }) => file_path)
         .then(path => fetch(`https://api.telegram.org/file//bot${token}/${path}`))
         .then(response => response.blob())
-        // .then(response => response)
         .then(audio => {
-            console.log(audio)
             return fetch(`http://asr.yandex.net/asr_xml?uuid=${uuid}&key=${yandexApiKey}&topic=queries&disableAntimat=false`, {
                 method: 'POST',
                 headers: {
@@ -49,5 +38,4 @@ bot.on('message', (ctx) => {
         .then(res => ctx.reply('Оно бы работало, но Илюша пока не может в аудио кодировки((('))
         .catch(err => console.log(err))
 });
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch()
