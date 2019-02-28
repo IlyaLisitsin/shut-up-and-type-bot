08fe9a9fcfa6ce5adc22598bf881e0bf235bab0b
Ilya Savchuk <ilyas.sporichev@gmail.com>
2019-02-28 22:40:04 +0300

    finished

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 const Telegraf = require('telegraf');
 const fetch = require("node-fetch");
+const fs = require('fs');
+const https = require('https');
 
-const token = '742079108:AAGjJ9Ugxp2uN4CTEepcdcFwpbXoc20LqzE';
-const uuid = '4a99246551f24c339dc20c7a415f3cd6';
-const yandexApiKey = '6891b0ef-ea2f-421a-a761-be94d69393ff';
-
-// fs.readFile('./file_50.oga', (err, data) =>
-//     fetch(`https://asr.yandex.net/asr_xml?uuid=${uuid}&key=${yandexApiKey}&topic=queries&disableAntimat=false`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'audio/ogg;codecs=opus',
-//             'Transfer-Encoding': 'chunked',
-//             'Host': 'asr.yandex.net',
-//         },
-//         body: new ArrayBuffer(data)
-//     })
-//         .then(kek => console.log(2, kek)))
+const token = '';       // Telegram token
+const catalogId = '';   // Yandex cloud folder id
+const iamToken = '';    // Yandex cloud IAM token
 
 const bot = new Telegraf(token);
 bot.on('message', (ctx) => {
     ctx.message.voice && fetch(`https://api.telegram.org/bot${token}/getFile?file_id=${ctx.message.voice.file_id}`)
         .then(response => response.json())
         .then(({ result: { file_path } }) => file_path)
-        .then(path => fetch(`https://api.telegram.org/file//bot${token}/${path}`))
-        .then(response => response.blob())
-        .then(audio => {
-            return fetch(`http://asr.yandex.net/asr_xml?uuid=${uuid}&key=${yandexApiKey}&topic=queries&disableAntimat=false`, {
-                method: 'POST',
-                headers: {
-                    'Host': 'asr.yandex.net',
-                    'Content-Type': 'audio/ogg;codecs=opus',
-                    'Transfer-Encoding': 'chunked',
-                },
-                body: audio
-            })
-        })
-        .then(res => ctx.reply('Оно бы работало, но Илюша пока не может в аудио кодировки((('))
+        .then(path => writeTheFile(`https://api.telegram.org/file//bot${token}/${path}`, () => sendResultToTheService(ctx)))
         .catch(err => console.log(err))
 });
 bot.launch()
+
+function sendResultToTheService(ctx) {
+    return fs.readFile('./file.oga', (err, data) => {
+        if (err) return err;
+        return fetch(`https://stt.api.cloud.yandex.net/speech/v1/stt:recognize/?topic=general&folderId=${catalogId}`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${iamToken}`,
+                'Transfer-Encoding': 'chunked',
+            },
+            body: data,
+        })
+            .then(res => res.json())
+            .then(({ result }) => ctx.reply(result))
+            .catch(err => console.log(err))
+    });
+
+}
+
+function writeTheFile(url, onWriteFinishedCb) {
+    const file = fs.createWriteStream("file.oga");
+    https.get(url, (response) => response.pipe(file));
+    file.on('finish', () => file.close(onWriteFinishedCb));
+}
